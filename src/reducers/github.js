86de6d4ecb26faf18dcee8e19586ca8de8a79fad
Valery Bugakov <skymk1@gmail.com>
2016-02-26 23:18:43 +0400

    switch to merge from Object.assign to save nested state props

--- src/reducers/github.js
+++ src/reducers/github.js
@@ -1,10 +1,11 @@
 import * as ActionTypes from 'actions/github'
 import { combineReducers } from 'redux'
+import merge from 'lodash/merge'
 import paginate from './paginate'
 
 export function entities(state = { users: {}, repos: {} }, action) {
   if (action.response && action.response.entities) {
-    return Object.assign({}, state, action.response.entities)
+    return merge({}, state, action.response.entities)
   }
   return state
 }
